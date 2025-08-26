8c8bf43866b6c21bcca8c3b664a822083ea7f70e
Matias Meza <153043791+MatSosa0@users.noreply.github.com>
2025-08-25 21:52:29 -0300

    clase 4

---
+++ Diseño de aplicaciones/proyecto012/src/app/app.spec.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ReactiveFormsModule],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {
+  resultado = '';
+  formAlumno = new FormGroup({
+dni: new FormControl(''),
+    nombre: new FormControl(''),
+    notas: new FormGroup({
+      nota1: new FormControl(''),
+      nota2: new FormControl(''),
+      nota3: new FormControl('')
+    })
+  });
+  submit() {
+    if (this.formAlumno.value.notas) {
+      if (this.formAlumno.value.notas.nota1 &&
+        this.formAlumno.value.notas.nota2 &&
+        this.formAlumno.value.notas.nota3) {
+        let nota1 = parseInt(this.formAlumno.value.notas.nota1);
+        let nota2 = parseInt(this.formAlumno.value.notas.nota2);
+        let nota3 = parseInt(this.formAlumno.value.notas.nota3);
+        if (nota1 >= 4 && nota2 >= 4 && nota3 >= 4)
+          this.resultado = "El alumno queda aprobado por esas notas" + " " + this.formAlumno.value.nombre;
+        else
+          this.resultado = "El alumno no aprueba por esas notas"+ " " + this.formAlumno.value.nombre;
+      }
+    }
+  }
+}
